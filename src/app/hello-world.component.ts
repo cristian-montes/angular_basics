80c28db0dc99681ea04ef7fba22237d413410156
Cristian Montes <cmontesmujica@gmail.com>
2022-07-18 11:03:48 -0600

    added a interva, function to put to action the lifeCyCleHooks

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
     template: '<h1>{{title}}</h1>',
     styles: [`
         h1 {
-            color: blue;
+            color: blue
         }
     `]
 })
